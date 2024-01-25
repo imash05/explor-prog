72dae96874938e04afcc5cadfabaab6290366468
IFer <76787367+imash05@users.noreply.github.com>
2024-01-26 00:03:35 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,27 +1,26 @@
-import * as React from 'react';
-import * as  ReactDOM from 'react-dom/client';
-import {createBrowserRouter,RouterProvider} from 'react-router-dom'
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
 const router = createBrowserRouter([
   {
-    path: '/',
-    element: <div>Test react router </div>
+    path: "/",
+    element: <div>Test react router </div>,
   },
   {
-    path: '/n',
-    element: <div>Test react router 22 </div>
+    path: "/n",
+    element: <div>Test react router 22 </div>,
   },
-
-])
+]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <RouterProvider router={router} />
   </React.StrictMode>
-)
+);
 
 reportWebVitals();
+
+// TODO Puoi fornire una UX decisamente migliore di questa quando la tua app genera errori fornendone una tua ErrorBoundaryO errorElementsostegno sul tuo percorso.
